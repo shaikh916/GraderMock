a150dd6f13962cd84343d4bef182e1ada2bf659a
Marco Meijer <marcomeijerm@gmail.com>
2024-02-14 15:00:58 +0100

    refactor: added display_verdicts function

--- src/app.py
+++ src/app.py
@@ -4,6 +4,16 @@ from streamlit_ace import st_ace
 from engine import run_code
 from submission import submissions, Submission
 
+def display_verdicts(verdicts: str | list[str]):
+    if verdicts == "CE":
+        st.error("Compilation Error")
+    else:
+        for idx, verdict in enumerate(verdicts):
+            if verdict == "AC":
+                st.success(f"Testcase {idx + 1}: {verdict}")
+            else:
+                st.error(f"Testcase {idx + 1}: {verdict}")
+
 code = st_ace( "Your code goes here...",
     language="python",
     font_size=13,
@@ -18,27 +28,12 @@ if st.button("Submit"):
 
     verdicts = run_code(code, problem_path="example_problem")
     submissions.append(Submission(code, verdicts))
-
-    if verdicts == "CE":
-        st.error("Compilation Error")
-    else:
-        for idx, verdict in enumerate(verdicts):
-            if verdict == "AC":
-                st.success(f"Testcase {idx + 1}: {verdict}")
-            else:
-                st.error(f"Testcase {idx + 1}: {verdict}")
+    display_verdicts(verdicts)
 
 if len(submissions) != 0:
     st.write("Submissions")
 for i, submission in list(enumerate(submissions))[::-1]:
     with st.expander(f"Submission #{i + 1} -------- {submission.time.strftime('%B %d, %Y, %I:%M:%S')}"):
         st_ace(submission.source_code, key=i, language="python", font_size=11, theme="chrome", readonly=True, auto_update=True)
-        if submission.verdicts == "CE":
-            st.error("Compilation Error")
-        else:
-            for idx, verdict in enumerate(submission.verdicts):
-                if verdict == "AC":
-                    st.success(f"Testcase {idx + 1}: {verdict}")
-                else:
-                    st.error(f"Testcase {idx + 1}: {verdict}")
+        display_verdicts(submission.verdicts)
 
